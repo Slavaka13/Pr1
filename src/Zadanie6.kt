a2aea2b2380350a5a5c7292a676ceee51822ab18
de12161 <gigorborozdin@gmail.com>
2024-09-12 17:20:37 +0300

    Добавить обрабоку ошибок ввода

--- src/Zadanie6.kt
+++ src/Zadanie6.kt
@@ -1,8 +1,15 @@
 fun main() {
-    print("Введите число a: ")
-    val i1 = readln().toInt()
-    print("Введите число b: ")
-    val i2 = readln().toInt()
+    val i1:Int
+    val i2:Int
+    try {
+        print("Введите число a: ")
+        i1 = readln().toInt()
+        print("Введите число b: ")
+        i2 = readln().toInt()
+    } catch (e:NumberFormatException) {
+        println("Ошибка ввода")
+        return
+    }
 
     if (i1 % 2 == 1) println(i1)
     else if (i2 % 2 == 1) println(i2)
